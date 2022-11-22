e96ac45b24cb3c462fd39ed628e779969120a989
192110448 <112369105+192110448@users.noreply.github.com>
2022-11-22 11:20:51 +0530

    Add files via upload

---
+++ for loop.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=1;i<=1;printf(" WELCOME\n",i++));
+	return 0;
+}
---
+++ while.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("WELCOME:");
+	
+	scanf("%d",&n);
+	
+	while(i<n)
+	{
+		printf("WELCOME\n");
+		i++;
+	}
+	printf("end");
+	return 0;
+
+}
